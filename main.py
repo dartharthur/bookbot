2226fbd2a1c964056749b6d3a9018557538f492b
Kevin Arthur <kevin.arthur.roy@gmail.com>
2025-01-06 16:15:28 -0800

    Save solution

--- main.py
+++ main.py
@@ -2,48 +2,46 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
-    char_counts = get_character_counts(text)
-    print_report(book_path, num_words, char_counts)
+    chars_dict = get_chars_dict(text)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{num_words} words found in the document")
+    print()
+
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"The '{item['char']}' character was found {item['num']} times")
+
+    print("--- End report ---")
 
 def get_num_words(text):
     words = text.split()
     return len(words)
 
+def sort_on(d):
+    return d["num"]
+
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
+
+def get_chars_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
+
 def get_book_text(path):
     with open(path) as f:
         return f.read()
-
-def get_character_counts(text):
-    char_count_dict = {}
-    char_counts = []
-
-    def sort_on(dict):
-        return dict["count"]
-
-    for char in text:
-        lowered_char = char.lower()
-        if lowered_char in char_count_dict:
-            char_count_dict[lowered_char] += 1
-        else:
-            char_count_dict[lowered_char] = 1
-
-    for char_key in char_count_dict:
-        if char_key.isalpha():
-            char_counts.append({"char": char_key, "count": char_count_dict[char_key]})
-
-    char_counts.sort(reverse=True, key=sort_on)
-
-    return char_counts
-
-def print_report(book_path, num_words, char_counts):
-    print(f"--- Begin report of {book_path} ---")
-    print(f"{num_words} words found in the document")
-    print(f"\n")
-    for char_info in char_counts:
-        character = char_info["char"]
-        count = char_info["count"]
-        print(f"The '{character}' character was found {count} times")
-
-    print("--- End Report ---")
-
+    
 main()
